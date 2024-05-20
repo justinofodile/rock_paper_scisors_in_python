9b39e46815a533d99c328f6549bbb1680cfb5a57
Ofodile Justin <justinofodile@gmail.com>
2024-05-20 10:11:04 +0100

    Rock Paper Scisors init commmit

---
+++ R_P_S.py
@@ -0,0 +1,43 @@
+import random
+
+user_win = 0
+computer_win = 0
+ties = 0
+options = ['rock', 'paper', 'scissors']
+
+while True:
+    user_input = input("Rock, Paper, Scissors or Q to Quit? ").lower()
+    if user_input == 'q':
+        break
+    elif user_input not in options:
+        continue
+
+    random_number = random.randint(0,2)
+    computer_picked = options[random_number]
+    print(f'Computer picked {computer_picked}')
+
+    if user_input == 'rock' and computer_picked == 'paper':
+        print("You won!")
+        user_win += 1
+    elif user_input == 'paper' and computer_picked == 'scissors':
+        print("You won!")
+        user_win +=1
+    elif user_input == 'scissors' and computer_picked == 'rock':
+        print("You won!")
+        user_win +=1
+    elif user_input == 'rock' and computer_picked == 'rock':
+        print("There is a tie!")
+        ties += 1
+    elif user_input == 'paper' and computer_picked == 'paper':
+        print("There is a tie!")
+        ties += 1
+    elif user_input == computer_picked:
+        print("There is a tie!")
+        ties += 1
+    else:
+        print("Computer won!")
+        computer_win += 1
+print("User won {} times".format(user_win))
+print("Computer won {} times".format(computer_win))
+print("Computer and user had {} times".format(ties))
+
